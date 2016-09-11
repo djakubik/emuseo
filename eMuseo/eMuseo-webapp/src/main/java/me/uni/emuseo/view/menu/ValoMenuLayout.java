55dab176d0fd82d7d6afe69f8e0cf89a31c1e199
darian jakubik <darian.jakubik@gmail.com>
2016-09-11 17:59:56 +0200

    Deletion of wrongly inserted license information

--- eMuseo/eMuseo-webapp/src/main/java/me/uni/emuseo/view/menu/ValoMenuLayout.java
+++ eMuseo/eMuseo-webapp/src/main/java/me/uni/emuseo/view/menu/ValoMenuLayout.java
@@ -1,13 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2016 Darian Jakubik.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Lesser Public License v2.1
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * 
- * Contributors:
- *     Darian Jakubik - initial API and implementation
- ******************************************************************************/
 /*
  * Copyright 2000-2014 Vaadin Ltd.
  * 

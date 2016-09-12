f33ebfa663d37e71c3a824c434f2d27c0bee03ab
darian jakubik <darian.jakubik@gmail.com>
2016-09-12 17:27:29 +0200

    deletion of old angular experiment project

--- angularjs-rest-service/WebContent/js/controller.js
+++
@@ -1,9 +0,0 @@
-
-var app = angular.module('serviceDemoApp');
-app.controller('serviceCtrl',['DemoService','$scope',function(DemoService, $scope){
-       // calling service
-       DemoService.query(function(DemoService){
-              $scope.response=DemoService;
-
-       });
-}]);
